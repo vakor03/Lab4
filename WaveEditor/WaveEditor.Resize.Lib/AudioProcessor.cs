9fef0747b049198b5ba32dd69db5aa7b9b649bb0
Valerii_kor <myksv200@gmail.com>
2021-04-17 18:33:23 +0400

    Removed useless int

--- WaveEditor/WaveEditor.Resize.Lib/AudioProcessor.cs
+++ WaveEditor/WaveEditor.Resize.Lib/AudioProcessor.cs
@@ -100,7 +100,7 @@ namespace WaveEditor.Resize.Lib
 
         private double Interpolate(double y0, double y1, double x0, double x1, double x)
         {
-            return (int)(y0 + (y1 - y0) * (x - x0) / (x1 - x0));
+            return (y0 + (y1 - y0) * (x - x0) / (x1 - x0));
         }
     }
 }
